d9f35befbdf727c995f9fa759900bb3a37d32d65
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-06-02 14:38:29 +0000

    Done Research This Keyword

--- Java OOPs  by Coding Wallah /Mypackage1/test.java
+++ Java OOPs  by Coding Wallah /Mypackage1/test.java
@@ -1,14 +1,19 @@
 
 
 
- class test {
+public class test {
     public static void main(String[] args) {
-    Employee enginer = new Employee();
-    System.out.println(enginer.getph());
-    // System.out.println(enginer.getName());
 
-    System.out.println(enginer.getName());
+        Employee enginer = new Employee("Shivam", 21);
+        Employee enginer21 = new Employee();
 
+        System.out.println(enginer.getph());
+        System.out.println(enginer.getName());
 
+        enginer21.setname("Sankru");
+        enginer21.setph(22222);
+
+        System.out.println(enginer21.getName());
+        System.out.println(enginer21.getph());
     }
 }
--- Java OOPs  by Coding Wallah /Mypackage2/Employee.java
+++ Java OOPs  by Coding Wallah /Mypackage2/Employee.java
@@ -1,20 +1,38 @@
 class Employee {
 
-    private int empId=2;
-    private int ph=0;
+    private int empId = 2;
+    private int ph = 0;
     private String name;
-    private int basicSal=20000;
-    private int incentive=200;
-    private void countSal(){
-        System.out.println(basicSal+incentive);
+    private int basicSal = 20000;
+    private int incentive = 200;
+
+    Employee() {
+        ph = 222;
+        name = "sanket";
     }
 
-    // getter & setter 
- int getph(){
-    return ph;
- }
- String getName(){
-    return name;
+    Employee(String empName, int phoneNo) {
+        ph = phoneNo;
+        name = empName;
+    }
 
- }
+    private void countSal() {
+        System.out.println(basicSal + incentive);
+    }
+
+    int getph() {
+        return ph;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void setph(int phone) {
+        ph = phone;
+    }
+
+    void setname(String name) {
+        this.name = name;   // this is used to parameter passing is imp for interview ok
+    }
 }
