d458eb5b422293b56f591ab0b17a0bb89ee59590
Sanket Bhosale <bhosalesanket1507@gmail.com>
2026-06-22 14:23:42 +0000

    ok

---
+++ Test.java
@@ -0,0 +1,23 @@
+
+class Employee{
+
+    static  int empId=2;
+    int incentive = 200;
+    int salary = 20000;
+    
+    void countSalries(){
+    System.out.println(salary+incentive);
+    }
+}
+
+class Test{
+
+    public static void main(String[] args) {
+        
+        Employee myjob = new Employee(); // MEMORY ALOCATE USE  ACTUAL OBJECT HOLD A MEMORY 
+
+        System.out.println();
+ 
+        
+    }
+}
